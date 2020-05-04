eda64da250993083af9c58bd2adc0d3b878fb767
David Obodo <obododavid5@gmail.com>
2020-05-04 19:00:03 +0100

    create different plans

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,39 @@ import React from "react";
 import { AppContainer } from "./Appstyles";
 import PricingPlan from "./components/pricingPlan/pricingPlan";
 
+const Plans = [
+  {
+    name: "Bronze",
+    price: "$69.99",
+    benefits: [
+      "100mb Disk space",
+      "Unlimited download",
+      "Free one year support",
+      "Free 15gb linux hosting"
+    ]
+  },
+  {
+    name: "Silver",
+    price: "$99.99",
+    benefits: [
+      "400mb Disk space",
+      "Unlimited download",
+      "Free one year support",
+      "Free 15gb linux hosting"
+    ]
+  },
+  {
+    name: "Gold",
+    price: "$169.99",
+    benefits: [
+      "1gb Disk space",
+      "Unlimited download",
+      "Free one year support",
+      "Free 15gb linux hosting"
+    ]
+  }
+];
+
 const App = () => {
   return (
     <AppContainer>
